c07ab0464175cf566978fdc9f7788a749c627549
otar <oiluridze@gmail.com>
2023-07-18 13:04:55 +0400

    Matrix Transpose

---
+++ MatrixTranspose/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MatrixTranspose;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        int row = matrix.GetLength(0);
+        int col = matrix.GetLength(1);
+
+        int[,] transpose = new int[col, row];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                transpose[j, i] = matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Standart Matrix");
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Console.Write(matrix[i,j] + " ");
+            }
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Transposed Matrix");
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                Console.Write(transpose[i, j] + " ");
+            }
+        }
+
+        for (int k = 0;k < row;k++)
+        {
+            Console.Write(transpose[k,k]);
+        }
+    }
+}
+
